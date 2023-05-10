1e3cccff6784b7f5817dcced0bf12be7fe0ee2ec
조형우 <hwjo1479@gmail.com>
2023-05-10 16:35:42 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system!</h2>
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '조형우',
+  'birthday': '970409',
+  'gender': '남자',
+  'job': '백수'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '조형규',
+  'birthday': '941025',
+  'gender': '남자',
+  'job': '비행기메이커'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '조달이',
+  'birthday': '201405',
+  'gender': '여자',
+  'job': '댕댕'
+}]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+        customers.map(c => {
+          return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+            />
+        );
+      })
+    }   
     </div>
-  );
-}
+      );
+    }
+  }
 
 export default App;
